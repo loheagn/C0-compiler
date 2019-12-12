8ce3a61b378d2d7fdb023ee4e3de6ae05baf6be9
loheagn <linan_123@buaa.edu.cn>
2019-12-12 21:47:13 +0800

    添加自定义的异常类

--- src/main/java/com/loheagn/utils/CompileException.java
+++ src/main/java/com/loheagn/utils/CompileException.java
@@ -20,6 +20,24 @@ enum ExceptionString {
 /**
  * CompileException
  */
-public class CompileException {
+public class CompileException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2463834329445053438L;
+
+    private String message;
+    private Position position;
+
+    CompileException(String message, Position position) {
+        this.message = message;
+        this.position = position;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("Row\t").append(position.row).append(", column\t").append(position.column).append(": ").append(message).toString();
+    }
 
 }
